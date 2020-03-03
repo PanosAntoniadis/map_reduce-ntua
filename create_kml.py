eda371b840c9f8b7bac0394198c76c9b2d79c5cc
PanosAntoniadis <pantoniadis97@gmail.com>
2020-03-03 21:52:53 +0200

    Add kml files and images

---
+++ create_kml.py
@@ -0,0 +1,11 @@
+import simplekml
+
+kml = simplekml.Kml()
+
+with open('kmeans.res', 'r') as f:
+    for i, line in enumerate(f):
+        lon = float(line.split(" ")[0][1:]) 
+        lat = float(line.split(" ")[2].strip("\n")[:-1])
+        kml.newpoint(name="point "+str(i+1), coords=[(lon,lat)])
+
+kml.save("kmeans.kml")
